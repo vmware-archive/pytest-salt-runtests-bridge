b505072a38ac53b41071cc56cd9b9d3a461477cd
Pedro Algarvio <pedro@algarvio.me>
2019-06-12 23:13:28 +0100

    Bump version for release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-salt-runtests-bridge',
-    version='2019.6.13',
+    version='2019.6.14',
     author='Pedro Algarvio',
     author_email='pedro@algarvio.me',
     maintainer='Pedro Algarvio',
